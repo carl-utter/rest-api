870cc6a6fde7a566a6c0c89b3bbefa841a203400
Carl Utter <carl.utter@endurance.com>
2018-02-01 16:00:09 -0700

    FIX(index.js): mongo connector updated the mongo connector to eliminate the open() is depreicated warning skip

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.Promise = Promise;
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 120 } } });
+mongoose.connect(mongoUri, { useMongoClient: true, keepAlive: true });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
